7ad88a2b9509d94e1f03bc9c8a9c77f43884f759
pujanov69 <pujanov69@gmail.com>
2020-04-09 14:09:35 +0545

    changes

--- src/main/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplication.java
+++ src/main/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplication.java
@@ -1,5 +1,7 @@
 package com.cicd.cicdappliedtospringbootjavaapp;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,11 @@ public class CicdAppliedToSpringBootJavaAppApplication {
 	}
 	
 	public static void main(String[] args) {
-		SpringApplication.run(CicdAppliedToSpringBootJavaAppApplication.class, args);
+		//SpringApplication.run(CicdAppliedToSpringBootJavaAppApplication.class, args);
+		
+		 Network net = new Network(4,1,3,4);
+		 double[] output = net.calculate(0.2, 0.9, 0.3, 0.4);
+		 System.out.println(Arrays.toString(output));
 	}
 
 }
